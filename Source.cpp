2670cf8e6f8c346b3609310c8155d32c8ff86624
Vasilshift <Vasilshift@github.com>
2021-01-06 00:57:09 +0500

    Add project files.

---
+++ Source.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int x, i, s = 0;
+
+	cout << "Please, enter the value: ";
+	cin >> x;
+		
+
+	cout << "All numbers by which this number is divisible without remainder: " << " " << endl;
+
+	for (i = 0; i < x; i++) {
+
+		s = s + 1;
+
+		if (x % s == 0) {
+
+			cout << s << " ";
+		}
+
+	}
+
+	cout << endl;
+
+
+	return 0;
+}
